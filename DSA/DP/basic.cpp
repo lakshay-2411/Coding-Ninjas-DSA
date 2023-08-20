357609e7d6fa04c1fd301ae17440cc768b414c90
lakshay-2411 <lakshaynandwani06@gmail.com>
2023-08-20 12:17:49 +0530

    DP files updated successfully

--- DSA/DP/basic.cpp
+++ DSA/DP/basic.cpp
@@ -1,6 +1,172 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
 #include <climits>
 using namespace std;
+
+
+int numBT_3(int h){
+    long long *dp = new long long[h+1];
+    dp[0] = dp[1] = 1;
+    int mod = 1e9 + 7;
+    for(int i=2;i<=h;i++){
+        long long x = dp[i-1] % mod;
+        long long y = dp[i-2] % mod;
+        long long temp1 = (x * x) % mod;
+        long long temp2 = (2*x * y) % mod;
+        dp[i] = (temp1 + temp2) % mod;
+    }
+    return (int)dp[h];
+}
+
+int numBT_1helper(int h,vector<int> ans){
+    if(h <= 1){
+        return 1;
+    }
+    if(ans[h] != -1){
+        return ans[h];
+    }
+    long long int x = numBT_1helper(h-1,ans);
+    long long int y = numBT_1helper(h-2,ans);
+    long long int mod = 1e9 + 7;
+
+    long long int temp1 = (x * x) % mod;
+    long long int temp2 = (2 * x * y) % mod;
+    ans[h] = (temp1 + temp2) % mod;
+    return ans[h];
+
+}
+
+int numBT_1(int h){
+    vector<int> ans(h+1,-1);
+    return numBT_1helper(h,ans);
+}
+
+long long int numBT(int h){
+    if(h <= 1){
+        return 1;
+    }
+
+    long long int x = numBT(h-1);
+    long long int y = numBT(h-2);
+    long long int mod = 1e9 + 7;
+
+    long long int temp1 = (x * x) % mod;
+    long long int temp2 = (2 * x * y) % mod;
+    long long int ans = (temp1 + temp2) % mod;
+
+    //int ans = (l*l + 2*l*r)%mod;
+    return ans;
+}
+
+int minCount_3(int n){
+    vector<int> dp(n+1,INT_MAX);
+
+    dp[0] = 0;
+
+    for(int i=1;i<=n;i++){
+        int j = 1;
+        while(j * j <= i){
+            dp[i] = min(dp[i],dp[i-j*j]+1);
+            j++;
+        }
+    }
+    return dp[n];
+}
+
+
+int minCount_2helper(int n,int *ans){
+    if(n == 0){
+        return 0;
+    }
+    if(ans[n] != -1){
+        return ans[n];
+    }
+    int res = INT_MAX;
+    for(int i=1;i*i<=n;i++){
+        ans[n] = res = min(res,minCount_2helper(n-i*i,ans));
+    }
+    ans[n] = res + 1;
+    return ans[n];
+}
+
+int minCount_2(int n){
+    int *ans = new int[n+1];
+    for(int i=0;i<=n;i++){
+        ans[i] = -1;
+    }
+    return minCount_2helper(n,ans);
+}
+
+int minCount(int n){
+    if(n <= 3){
+        return n;
+    }
+    int res = INT_MAX;
+    for(int i=1;i*i<=n;i++){
+        res = min(res,minCount(n - i * i));
+    }
+    return res + 1;
+}
+
+
+//-------------------------------------------STAIRCASE-------------------------------------------//
+
+
+//Method 3--------------DYNAMIC PROGRAMMING(Bottom Up Approach)---------------Time Complexity : O(N)
+int stairs_3(int n){
+    if(n == 0 || n == 1){
+        return 1;
+    }
+    int *ans = new int[n+1];
+    ans[0] = 1;
+    ans[1] = 1;
+    ans[2] = 2;
+    for(int i=3;i<=n;i++){
+        ans[i] = ans[i-1] + ans[i-2] + ans[i-3];
+    }
+    return ans[n];
+}
+
+//Method 2-----------------MEMOIZATION(Top to Bottom Approach)--------------Time Complexity : O(N)
+int stairs_2helper(int n,int *ans){
+    if(n < 0){
+        return 0;
+    }
+    if(n == 0){
+        return 1;
+    }
+    if(ans[n] != -1){
+        return ans[n];
+    }
+    int x = stairs_2helper(n-1,ans);
+    int y = stairs_2helper(n-2,ans);
+    int z = stairs_2helper(n-3,ans);
+
+    ans[n] = x+y+z;
+
+    return ans[n];
+}
+
+int stairs_2(int n){
+    int *ans = new int[n+1];
+    for(int i=0;i<=n;i++){
+        ans[i] = -1;
+    }
+    return stairs_2helper(n,ans);
+}
+
+//Method 1--------------------BRUTE_FORCE_APPROACH-------------------------Time Complexity : O(3^n)
+int stairs(int n){
+    if(n < 0){
+        return 0;
+    }
+    if(n == 0){
+        return 1;
+    }
+    return stairs(n-1) + stairs(n-2) + stairs(n-3);
+}
+
 
 //--------------------------------------MIN_STEPS_TO_1-------------------------------------------//
 
@@ -27,7 +193,7 @@ int minSteps3(int n){
     return ans[n];
 }
 
-//Method 2-----------------MEMORIZATION(Top to Bottom Approach)--------------Time Complexity : O(N)
+//Method 2-----------------MEMOIZATION(Top to Bottom Approach)--------------Time Complexity : O(N)
 int minSteps2_helper(int n,int *ans){
     if(n <= 1){
         return 0;
@@ -98,7 +264,7 @@ int fibo_3(int n){
     return ans[n];
 }
 
-//Method 2-----------------MEMORIZATION(Top to Bottom Approach)---------------Time Complexity : O(N)
+//Method 2-----------------MEMOIZATION(Top to Bottom Approach)---------------Time Complexity : O(N)
 int fibo_2helper(int n,int *ans){
     if(n <= 1){
         return n;
@@ -138,6 +304,11 @@ int fibo(int n){
 }
 
 int main(){
-    cout << minSteps3(7);
+    int n;
+    cin >> n;
+    cout << numBT_3(n);
+    //cout << minCount_3(1000);
+    //cout << stairs(4);
+    //cout << minSteps3(7);
     //cout << fibo_3(7);
 }
--- DSA/Tries and Hufffman Coding/TrieUse.cpp
+++ DSA/Tries and Hufffman Coding/TrieUse.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //#include "TrieClass.h"
 //#include "autoComplete.h"
 #include "palindromePair.h"
@@ -71,78 +70,4 @@ int main(){
     // t.removeWord("bye");
     // cout << t.searchWord("bye");
 
-=======
-//#include "TrieClass.h"
-//#include "autoComplete.h"
-#include "palindromePair.h"
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int main(){
-    Trie t;
-    int n;
-    cin >> n;
-
-    vector<string> v(n);
-
-    for(int i=0;i<n;i++){
-        cin >> v[i];
-    }
-
-    cout << (t.isPalindrome(v) ? "true" : "false");
-
-
-
-
-
-
-
-
-
-/*
-    vector<string> v;
-    for(int i=0;i<n;i++){
-        string word;
-        cin >> word;
-        v.push_back(word);
-    }
-    string pattern;
-    cin >> pattern;
-
-    t.autoComplete(v,pattern);
-
-*/
-
-
-
-
-
-
-    /*
-    t.insertWord("abc");
-    t.insertWord("def");
-    t.insertWord("ghi");
-    t.insertWord("cba");
-    t.insertWord("bye");
-    t.insertWord("no");
-    */
-
-
-/*
-    t.insertSuffixes("abc");
-    t.insertSuffixes("def");
-    t.insertSuffixes("ghi");
-    t.insertSuffixes("cba");
-    t.insertSuffixes("bye");
-    t.insertSuffixes("no");
-
-    cout << t.searchPrefix("hi") << endl;
-
-*/    
-
-    // t.removeWord("bye");
-    // cout << t.searchWord("bye");
-
->>>>>>> 337a89362980de6a6fedba5a5f8e98349bdc19f7
 }
